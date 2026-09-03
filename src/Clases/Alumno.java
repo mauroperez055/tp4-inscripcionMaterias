38f25f99c5d9206887bb5baee233f5d6b523fc40
Mauro Gabriel Perez <mauroperez055@gmail.com>
2026-09-03 16:37:09 -0300

    clases y metodos implementados

---
+++ src/Clases/Alumno.java
@@ -0,0 +1,54 @@
+
+package Clases;
+
+import java.util.HashSet;
+
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    private HashSet<Materia> materiasInscripto = new HashSet<>();
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public HashSet<Materia> getMateriasInscripto() {
+        return materiasInscripto;
+    }
+    
+    public void agregarMateria(Materia m) {
+        materiasInscripto.add(m);
+    }
+    
+    public int cantidadMaterias() {
+        System.out.print("Cantidad de materias de " + apellido + ": ");
+        return materiasInscripto.size();
+    }
+}
--- src/Clases/Colegio.java
+++ src/Clases/Colegio.java
@@ -1,21 +1,25 @@
 package Clases;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author Mauro Perez
- */
 public class Colegio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Materia web2 = new Materia(1, "Web 2", 2);
+        Materia mate = new Materia(2, "Matemáticas", 1);
+        Materia lab1 = new Materia(3, "Laboratorio 1", 1);
+        Alumno alu1 = new Alumno(1001, "López", "Martin");
+        Alumno alu2 = new Alumno(1002, "Martínez", "Brenda");
+        
+        alu1.agregarMateria(web2);
+        alu1.agregarMateria(mate);
+        alu1.agregarMateria(lab1);
+        
+        alu2.agregarMateria(web2);
+        alu2.agregarMateria(mate);
+        alu2.agregarMateria(lab1);
+        alu2.agregarMateria(lab1);
+        
+        System.out.println(alu1.cantidadMaterias());
+        System.out.println(alu2.cantidadMaterias());
     }
     
 }
